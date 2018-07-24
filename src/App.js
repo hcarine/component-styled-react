38c3dd743c70266c04b9affcb812d1d044fc425c
hcarine <hammescarine@gmail.com>
2018-07-24 18:25:15 +0200

    refactoring and create 2 bottons

--- src/App.js
+++ src/App.js
@@ -9,112 +9,73 @@ const Title = styled.h1`
 `;
 
 const Button = styled.button`
-  background: ${props=> props.theme ? 'orange': 'green'}
+  background: ${ props => props.theme.background}
 `
 
-const Button2 = styled.button`
-  background-color: ${props=> props.theme2.skyTest}
-`
-const Sky = styled.div`
-  background-color: ${props => props.theme.skyTest}
+const ButtonToggle = styled.div`
+  background-color: ${props => props.theme.background}
 `;
 
-
 const inativeTheme = {
-  skyTest: "#37d8e6"
+  background: 'orange'
 };
 const activeTheme = {
-  skyTest:'green'
- }
-
+  background:'green'
+}
 
 class App extends Component {
   constructor(props){
     super(props)
-
     this.state = {
-      //theme: true,
-      isDay: true,
+      isActive: true,
       theme2: inativeTheme,
+      theme: activeTheme,
       btnText: 'submit'
     }
-
-    //this.handleClick = this.handleClick.bind(this)
-    //this.handleClick2 = this.handleClick2.bind(this)
+    this.handleClick = this.handleClick.bind(this)
   }
 
   handleClick(event){
-    this.btn ='submitted'
-    this.setState({theme : false})
+    const isActive = !this.state.isActive;
+    this.setState({
+      isActive: isActive,
+      theme: isActive ? activeTheme : inativeTheme,
+      btnText: isActive ? 'submit': 'submitted'
+    })
     event.preventDefault()
   }
 
   handleClick2(){
-    const isDay = !this.state.isDay;
+    const isActive = !this.state.isActive;
 
     this.setState({
-      isDay: isDay,
-      theme2: isDay ? inativeTheme : activeTheme,
-      btnText: 'submitted'
+      isActive: isActive,
+      theme2: isActive ? inativeTheme : activeTheme,
+      btnText: isActive ? 'submit': 'submitted'
     })
-    //event.preventDefault()
   }
 
 
   render() {
-    console.log(this.state.theme)
     return (
-  
-     
-      <ThemeProvider theme = {this.state.theme2} className="App">
-  
-      <Sky onClick={() => this.handleClick2()}>
-        {this.state.btnText}
-        </Sky>
+      <div> 
+        <ThemeProvider theme = {this.state.theme2} className="App">
+          <ButtonToggle onClick={() => this.handleClick2()}>
+            {this.state.btnText}
+          </ButtonToggle>
+        </ThemeProvider>
 
-      </ThemeProvider>
-    );
-  }
-}
-/* <ThemeProvider theme = {this.state.theme2} className="App">
-          <Button2 type="Submit"  onClick={() =>this.handleClick2()} >
-            ooiiiii
-          </Button2>
-         
-      </ThemeProvider>*/
-
-export default App;
-
-/*
-
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Title>Welcome to React :p
-          </Title>
-        </header>
-        <form>
-        <label>
-          Nome
-          <input/>
-        </label>
-        <Button type="Submit" theme={this.state.theme} onClick={this.handleClick} >
-          {this.btn}
-        </Button>
-
-        <ThemeProvider theme={this.state.theme2}>
-          <Button type="Submit"  onClick={() =>this.handleClick2()} >
-            ooiiiii
+        <ThemeProvider theme = {this.state.theme}>
+          <Button onClick={this.handleClick}>
+             {this.state.btnText}
           </Button>
+          
         </ThemeProvider>
 
 
-        <button>teste</button>
-
-        </form>
-
       </div>
+    );
+  }
+}
 
-
-
-*/
+export default App;
